939952e42f5f365208cc84c5cd6c739de09524b4
Yulia Elagina <julia-elagina@yandex.ru>
2021-06-12 23:22:54 +0300

    Создает компонент для поля ввода суммы и добавляет логику конвертации

--- project/src/components/converter/converter.jsx
+++ project/src/components/converter/converter.jsx
@@ -1,51 +1,106 @@
-import React from "react";
+import React, {PureComponent} from "react";
+import InputField from "../input-field/input-field";
 
-const Converter = (props) => {
-  return (
-          <section className="converter">
-        <h1 className="converter__title">Конвертер валют</h1>
-        <form action="#" className="convert-form" method="GET">
-          <div className="convert-form__wrapper">
-            <div className="convert-form__field field">
-              <label className="field__label" htmlFor="sale">У меня есть</label>
-              <div className="field__wrapper">
-                <input className="field__control field__control--input" type="number" id="sale" name="sale" defaultValue="1000" />
-                <select className="field__control field__control--select" name="currency" id="currency-sale" defaultValue="rub">
-                  <option value="rub">RUB</option>
-                  <option value="usd">USD</option>
-                  <option value="eur">EUR</option>
-                  <option value="gbr">GBR</option>
-                  <option value="cny">CNY</option>
-                </select>
-              </div>
-              <label className="visually-hidden" htmlFor="currency-sale">Выберите валюту</label>
+const getCurrentRate = (rate, saleCurrency, buyCurrency) => {
+  return Number(rate[`${saleCurrency}${buyCurrency}`]);
+};
+
+export default class Converter extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      saleSum: 1000,
+      saleCurrency: 'RUB',
+      buySum: 13.1234,
+      buyCurrency: 'USD',
+      date: '1.12.2020',
+      currentRate: getCurrentRate(props.rate, 'RUB', 'USD'),
+    }
+
+    this._onBuyCurrencyChange = this._onBuyCurrencyChange.bind(this);
+    this._onBuySumChange = this._onBuySumChange.bind(this);
+    this._onSaleCurrencyChange = this._onSaleCurrencyChange.bind(this);
+    this._onSaleSumChange = this._onSaleSumChange.bind(this);
+  }
+
+  _onBuySumChange(evt) {
+    const buySum = Number(evt.target.value) || '';
+    const saleSum = (buySum) ? (buySum / this.state.currentRate).toFixed(2) : '';
+    this.setState({
+      buySum,
+      saleSum
+    });
+  }
+
+  _onBuyCurrencyChange(evt) {
+    const {saleCurrency, buySum} = this.state;
+    const {rate} = this.props;
+    const buyCurrency = evt.target.value;
+    const currentRate = getCurrentRate(rate, saleCurrency, buyCurrency);
+    const saleSum = (buySum) ? (buySum / currentRate).toFixed(2) : '';
+    this.setState({
+      buyCurrency,
+      currentRate,
+      saleSum,
+    });
+  }
+
+  _onSaleSumChange(evt) {
+    const saleSum = Number(evt.target.value) || '';
+    const buySum = (saleSum) ? (saleSum * this.state.currentRate).toFixed(4) : '';
+    this.setState({
+      buySum,
+      saleSum,
+    });
+  }
+
+  _onSaleCurrencyChange(evt) {
+    const {buyCurrency, saleSum} = this.state;
+    const {rate} = this.props;
+    const saleCurrency = evt.target.value;
+    const currentRate = getCurrentRate(rate, saleCurrency, buyCurrency);
+    const buySum = (saleSum) ? (saleSum / currentRate).toFixed(2) : '';
+    this.setState({
+      saleCurrency,
+      currentRate,
+      buySum,
+    });
+  }
+
+  render() {
+    const {buySum, buyCurrency, saleSum, saleCurrency} = this.state;
+    return (
+      <section className="converter">
+          <h1 className="converter__title">Конвертер валют</h1>
+          <form action="#" className="convert-form" method="GET">
+            <div className="convert-form__wrapper">
+              <InputField
+                name="sale"
+                labelText="У меня есть"
+                currency={saleCurrency}
+                value={saleSum}
+                onCurrencyChange={this._onSaleCurrencyChange}
+                onValueChange={this._onSaleSumChange}
+              />
+              <img className="convert-form__sign" src="img/arrows.svg" width="50" height="34" alt="Конвертировать валюту"/>
+              <InputField
+                name="buy"
+                labelText="Хочу купить"
+                currency={buyCurrency}
+                value= {buySum}
+                onCurrencyChange={this._onBuyCurrencyChange}
+                onValueChange={this._onBuySumChange}
+              />
             </div>
-            <img className="convert-form__sign" src="img/arrows.svg" width="50" height="34" alt="Конвертировать валюту"/>
-            <div className="convert-form__field field">
-              <label className="field__label" htmlFor="buy">Хочу приобрести</label>
-              <div className="field__wrapper">
-                <input className="field__control field__control--input" type="number" id="buy" name="buy" defaultValue="13.1234" />
-                <select className="field__control field__control--select" name="currency" id="currency-buy" defaultValue="usd">
-                  <option value="rub">RUB</option>
-                  <option value="usd">USD</option>
-                  <option value="eur">EUR</option>
-                  <option value="gbr">GBR</option>
-                  <option value="cny">CNY</option>
-                </select>
-              </div>
-              <label className="visually-hidden" htmlFor="currency-buy">Выберите валюту</label>
+            <div className="convert-form__wrapper">
+              <p className="convert-form__field field">
+                <label className="visually-hidden" htmlFor="date">Дата для расчета курса валют</label>
+                <input className="field__control field__control--input field__control--date" type="text" id="date" name="date" defaultValue="1.12.2020" />
+              </p>
+              <button className="convert-form__button button" type="submit">Сохранить результат</button>
             </div>
-          </div>
-          <div className="convert-form__wrapper">
-            <p className="convert-form__field field">
-              <label className="visually-hidden" htmlFor="date">Дата для расчета курса валют</label>
-              <input className="field__control field__control--input field__control--date" type="text" id="date" name="date" defaultValue="1.12.2020" />
-            </p>
-            <button className="convert-form__button button" type="submit">Сохранить результат</button>
-          </div>
-        </form>
-      </section>
-  );
+          </form>
+        </section>
+    );
+  }
 }
-
-export default Converter;
---
+++ project/src/components/input-field/input-field.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const InputField = (props) => {
+  const {name, currency, value, onCurrencyChange, onValueChange, labelText} = props;
+
+  return (
+    <div className="convert-form__field field">
+      <label className="field__label" htmlFor={name}>{labelText}</label>
+      <div className="field__wrapper">
+        <input className="field__control field__control--input" onChange={onValueChange} type="number" id={name} name={name} value={value} />
+        <select className="field__control field__control--select" onChange={onCurrencyChange} name={`currency-${name}`} id={`currency-${name}`} value={currency}>
+          <option value="RUB">RUB</option>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="GBP">GBP</option>
+          <option value="CNY">CNY</option>
+        </select>
+      </div>
+      <label className="visually-hidden" htmlFor={`currency-${name}`}>Выберите валюту</label>
+    </div>
+  );
+}
+
+InputField.propTypes = {
+  currency: PropTypes.string.isRequired,
+  labelText: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  onCurrencyChange: PropTypes.func.isRequired,
+  onValueChange: PropTypes.func.isRequired,
+  value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+};
+
+export default InputField;
