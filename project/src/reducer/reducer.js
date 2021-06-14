fdd7f2cfcbd128509bf54ad647b8ccc47023d20a
Yulia Elagina <julia-elagina@yandex.ru>
2021-06-14 22:36:55 +0300

    Добавляет асинхронные Action в reducer, удаляет моковые данные

--- project/src/reducer/reducer.js
+++ project/src/reducer/reducer.js
@@ -1,113 +1,23 @@
 import {extend} from "../utils";
 import {MAX_OPERATIONS_COUNT} from "../const";
 
-const rate = {
-  "CNYCNY": "1.000000",
-  "CNYEUR": "0.128256",
-  "CNYGBP": "0.110694",
-  "CNYRUB": "11.525354",
-  "CNYUSD": "0.156698",
-  "EURCNY": "7.796876",
-  "EUREUR": "1.000000",
-  "EURGBP": "0.863066",
-  "EURRUB": "89.861752",
-  "EURUSD": "1.221755",
-  "GBPCNY": "9.033930",
-  "GBPEUR": "1.158660",
-  "GBPGBP": "1.000000",
-  "GBPRUB": "104.119243",
-  "GBPUSD": "1.415598",
-  "RUBCNY": "0.086765",
-  "RUBEUR": "0.011128",
-  "RUBGBP": "0.009604",
-  "RUBRUB": "1.000000",
-  "RUBUSD": "0.013596",
-  "USDCNY": "6.381704",
-  "USDEUR": "0.818495",
-  "USDGBP": "0.706415",
-  "USDRUB": "73.551395",
-  "USDUSD": "1.000000",
-};
-
-const operations = [
-  {
-    id: 0,
-    date: '2021-06-12',
-    perchaseSum: 13.1234,
-    perchaseCurrency: 'USD',
-    saleSum: 1000,
-    saleCurrency: 'RUB',
-  },
-  {
-    id: 1,
-    date: '2021-06-11',
-    perchaseSum: 13.1234,
-    perchaseCurrency: 'GBP',
-    saleSum: 1000,
-    saleCurrency: 'RUB',
-  },
-  {
-    id: 2,
-    date: '2021-06-10',
-    perchaseSum: 13.1234,
-    perchaseCurrency: 'USD',
-    saleSum: 1000,
-    saleCurrency: 'RUB',
-  },
-  {
-    id: 3,
-    date: '2021-06-09',
-    perchaseSum: 13.1234,
-    perchaseCurrency: 'USD',
-    saleSum: 500,
-    saleCurrency: 'CNY',
-  },
-  {
-    id: 4,
-    date: '2021-06-08',
-    perchaseSum: 33.1234,
-    perchaseCurrency: 'USD',
-    saleSum: 2000,
-    saleCurrency: 'RUB',
-  },
-  {
-    id: 5,
-    date: '2021-06-07',
-    perchaseSum: 13.1234,
-    perchaseCurrency: 'USD',
-    saleSum: 1000,
-    saleCurrency: 'RUB',
-  },
-  {
-    id: 6,
-    date: '2021-06-06',
-    perchaseSum: 11.1234,
-    perchaseCurrency: 'EUR',
-    saleSum: 1000,
-    saleCurrency: 'RUB',
-  },
-  {
-    id: 7,
-    date: '2021-06-05',
-    perchaseSum: 13.1234,
-    perchaseCurrency: 'USD',
-    saleSum: 1000,
-    saleCurrency: 'RUB',
-  },
-];
-
 
 const initialState = {
   date: new Date(),
-  rate,
-  operations,
+  isLoading: false,
+  hasError: false,
+  operations: [],
+  rate: {}
 }
 
 const ActionType = {
   ADD_OPERATION: 'ADD_OPERATION',
   CLEAR_OPERATIONS: 'CLEAR_OPERATIONS',
+  END_LOADING: 'END_LOADING',
+  LOAD_RATE: 'LOAD_RATE',
+  SET_ERROR: 'SET_ERROR',
   SET_DATE: 'SET_DATE',
-  SET_RATE: 'SET_RATE',
+  START_LOADING: 'START_LOADING',
 }
 
 const ActionCreator = {
@@ -120,15 +30,50 @@ const ActionCreator = {
     type: ActionType.CLEAR_OPERATIONS
   }),
 
+  endLoading: () => ({
+    type: ActionType.END_LOADING,
+  }),
+
+  loadRate: (rate) => ({
+    type: ActionType.LOAD_RATE,
+    payload: rate,
+  }),
+
   setDate: (date) => ({
     type: ActionType.SET_DATE,
     payload: date,
   }),
 
   setRates: (rate) => ({
-    type: ActionType.SET_RATE,
+    type: ActionType.LOAD_RATE,
     payload: rate,
+  }),
+
+  startLoading: () => ({
+    type: ActionType.START_LOADING,
+  }),
+
+  setError: (hasError) => ({
+    type: ActionType.SET_ERROR,
+    payload: hasError,
   })
+};
+
+const Operation = {
+  loadRate: (date) => (dispatch, getState, api) => {
+    dispatch(ActionCreator.startLoading());
+    dispatch(ActionCreator.setError(false));
+    return api.getRates(date)
+    .then((response) => {
+      dispatch(ActionCreator.loadRate(response));
+      dispatch(ActionCreator.endLoading());
+    })
+    .catch((err) => {
+      dispatch(ActionCreator.endLoading());
+      dispatch(ActionCreator.setError(true));
+      return err;
+    });
+  },
 };
 
 const reducer = (state = initialState, action) => {
@@ -138,16 +83,39 @@ const reducer = (state = initialState, action) => {
       return extend(state, {
         operations: [action.payload, ...operations]
       });
+
     case ActionType.CLEAR_OPERATIONS:
       return extend(state, {
         operations: []
-      })
+      });
+
+    case ActionType.END_LOADING:
+      return extend(state, {
+        isLoading: false,
+      });
+
+    case ActionType.LOAD_RATE:
+      return extend(state, {
+        rate: action.payload
+      });
+
     case ActionType.SET_DATE:
       return extend(state, {
         date: action.payload,
       });
+
+    case ActionType.SET_ERROR:
+      return extend(state, {
+        hasError: action.payload,
+      });
+
+    case ActionType.START_LOADING:
+      return extend(state, {
+        isLoading: false,
+      });
+
     default: return state;
   }
 };
 
-export {ActionCreator, ActionType, reducer};
+export {ActionCreator, ActionType, Operation, reducer};
