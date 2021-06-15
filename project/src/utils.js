f7c39508513fb90f75efe5028382df0255e9f02c
Yulia Elagina <julia-elagina@yandex.ru>
2021-06-15 20:41:19 +0300

    Переносит магические значения в константы

--- project/src/utils.js
+++ project/src/utils.js
@@ -1,15 +1,16 @@
 import flatpickr from "flatpickr";
+import { DIGITS_NUMBER } from "./const";
 
 export const getCurrentRate = (rate, saleCurrency, perchaseCurrency) => {
   return parseFloat(rate[`${saleCurrency}${perchaseCurrency}`]);
 };
 
 export const convertSaleSum = (saleSum, currentRate) => {
-  return (saleSum) ? (saleSum * currentRate).toFixed(4) : '';
+  return (saleSum) ? parseFloat((saleSum * currentRate).toFixed(DIGITS_NUMBER)) : '';
 };
 
 export const convertPerchaseSum = (perchaseSum, currentRate) => {
-  return (perchaseSum) ? (perchaseSum / currentRate).toFixed(4) : '';
+  return (perchaseSum) ? parseFloat((perchaseSum / currentRate).toFixed(DIGITS_NUMBER)) : '';
 };
 
 export const extend = (a, b) => {
